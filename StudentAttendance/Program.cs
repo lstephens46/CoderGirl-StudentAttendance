86cc60b89b173ca7b189293cc53041c7055ef068
CailieAC <cailiecarlile@gmail.com>
2019-03-26 19:06:39 -0500

    Student Test

--- StudentAttendance/Program.cs
+++ StudentAttendance/Program.cs
@@ -4,14 +4,21 @@ namespace StudentAttendance
 {
     public static class Program
     {
+        //program so that the values for the student class are populated from the included studentdata.txt file. 
+        //The student’s name is the first thing on each line, followed by some exam scores. 
+        //The number of scores might be different for each student.  
+        //The program should print out the names of students that have more than six quiz scores.
+
         public static void Main()
         {
+            
             foreach (var student in students)
             {
                 Console.WriteLine($"{student.Name}");
             }
 
             Console.ReadLine();
+            
         }
     }
 }
--- StudentAttendance/Student.cs
+++ StudentAttendance/Student.cs
@@ -4,12 +4,22 @@ namespace StudentAttendance
 {
     public class Student
     {
+        //program so that the values for the student class are populated from the included studentdata.txt file. 
+        //The student’s name is the first thing on each line, followed by some exam scores. 
+        //The number of scores might be different for each student.  
+        //The program should print out the names of students that have more than six quiz scores.
+
         public string Name { get; set; }
 
         public int[] Scores { get; set; }
 
         public bool HasSixOrMore()
         {
+            if (Scores.Length >= 6)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
